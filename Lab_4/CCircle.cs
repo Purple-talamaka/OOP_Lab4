5212de27761d37d858dbe112d86aa18d3eea3c31
Purple-talamaka <arturtalamakovich@gmail.com>
2026-04-10 14:40:33 +0500

    Add CShape base class and CCircle

---
+++ Lab_4/CCircle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    public class CCircle : CShape
+    {
+        private int _radius;
+        public CCircle(int x, int y, int radius)
+        {
+            _x = x;
+            _y = y;
+            _radius = radius;
+        }
+
+        public override bool HitTest(int x, int y)
+        {
+            int dx = x - _x;
+            int dy = y - _y;
+            return dx * dx + dy * dy <= _radius * _radius;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            int left = _x - _radius;
+            int top = _y - _radius;
+            int size = _radius * 2;
+
+            if (_isSelected)
+            {
+                g.FillEllipse(Brushes.LightBlue, left, top, size, size);
+                g.DrawEllipse(new Pen(Color.Blue, 2), left, top, size, size);
+            }
+            else
+            {
+                g.FillEllipse(Brushes.LightGray, left, top, size, size);
+                g.DrawEllipse(Pens.Black, left, top, size, size);
+            }
+        }
+    }
+}
---
+++ Lab_4/CShape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    public abstract class CShape
+    {
+        protected int _x;
+        protected int _y;
+        protected Color _color;
+        protected bool _isSelected;
+
+        public void Move(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public abstract void Draw(Graphics g);
+
+        public abstract bool HitTest(int x, int y);
+    }
+}
